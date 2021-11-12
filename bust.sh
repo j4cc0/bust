af7ac4bd00d024f03f92b014d4ab604907c8212c
Jacco van Buuren <jacco.vanbuuren@tno.nl>
2021-11-12 17:31:57 +0100

    Implemented usage

--- bust.sh
+++ bust.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
+#=@ # BUST
+#=@  
+#=@ Proof-of-concept directory buster in bash using curl (and wget)
+#=@   
+#=@ # Usage
+#=@  
+#=@ `bust.sh -w "<some wordlist|number|0number>" -u "<some url>" -v -c -d -t 10 -s <seperator> -x '<.ext .ens .ion .s>' -m '<METHOD>'`
+#=@   
+#=@ ## Parameters
+#=@  
+#=@ -h				Help
+#=@ -w wordlist|number|0number	Wordlist (=file) to use. If a number (not a file), then count until this number. If the number starts with 0, pad the number with leading zero's.
+#=@ -u url			URL. Whatever url. This is PREpended to the words (or numbers) from -w
+#=@ -s seperator		Seperator. Often this is /. Sometimes a blank is needed to let a counter append to the URL. See examples.
+#=@ -x extensions		Extensions to check. Each word from wordlist has this extension appended. See examples.
+#=@ -t threads			Number of checks to perform in parallel. Should be numerical and greater than 0.
+#=@ -m method			A HTTP-method can be specified. Usually GET, HEAD or POST.
+#=@ -v				Verbose output. Also show responses other than 200 or 301/302.
+#=@ -c				Colorize output. Use fancy colors. It's 2021 you know.
+#=@ -d				Don't wait for the scan to complete and download those potential short lived files when found. I needed this.
+#=@  
+#=@ # Examples
+#=@  
+#=@ ~/bin/bust.sh -w 09999 -x '.zip' -s '' -m HEAD -cvu http://10.31.73.11/backups/backup_2021111010
+#=@ Append four zero padded digits up to 9999 after the URL and add .zip as extension. Use HEAD (instead of GET). Be verbose (showing 404s) and colorize the output.
+#=@  
+
 # -- Globals, constants
 
 WORDLIST="/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt"
@@ -22,6 +49,9 @@ EOC=""
 OK="[+]"
 NOK="[-]"
 WTF="[!]"
+
+DOXMARK='#.@ '
+SCRIPT="$0"
 
 # -- Functions
 
@@ -73,9 +103,10 @@ die() {
 }
 
 usage() {
-	cat <<-EOF
-	Sorry. Not implemented yet.
-	EOF
+	grep "$DOXMARK" "$SCRIPT" | sed -e "s/$DOXMARK//" -e 's/$/\n/' | grep -v '^DOXMARK=' | grep -v '^$'
+	#cat <<-EOF
+	#Sorry. Not implemented yet.
+	#EOF
 	return 0
 }
 
@@ -134,6 +165,7 @@ do
 		;;
         h)
                 usage
+		exit 0
                 ;;
 	p)
 		METHOD="POST"
